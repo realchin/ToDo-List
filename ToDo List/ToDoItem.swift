f565f7702355910b8a31ddcb0e08b7684cf46f00
Timothy Chin <chinti@bc.edu>
2022-02-18 15:26:54 -0500

    Section 5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Timothy Chin on 2/18/22.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
