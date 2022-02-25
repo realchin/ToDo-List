9d41c149d473fafb1b0c11106ae7c1960aa80882
Timothy Chin <chinti@bc.edu>
2022-02-25 12:41:21 -0500

    Section 5.10

--- ToDo List/AppDelegate.swift
+++ ToDo List/AppDelegate.swift
@@ -13,7 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        
         return true
     }
 
@@ -32,5 +35,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let id = notification.request.identifier
+        print("Received in-app notification with ID = \(id)")
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        completionHandler([.alert, .sound])
+    }
+    
 }
 
--- ToDo List/ToDoListViewController.swift
+++ ToDo List/ToDoListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ToDoListViewController: UIViewController {
     
@@ -24,6 +25,75 @@ class ToDoListViewController: UIViewController {
         tableView.dataSource = self
         //right after data loads
         loadData()
+        authorizeLocalNotifications()
+        
+    }
+    
+    func authorizeLocalNotifications() {
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error)
+            in
+            guard error == nil else {
+                print("🤬 ERROR: \(error!.localizedDescription)")
+                return
+            }
+            if granted {
+                print("✅ Notification Authorization Granted!")
+            } else {
+                print("🚫 The user has denied notifications!")
+                // TODO : Put an alert in here telling the user what to do
+            }
+        }
+        
+    }
+    
+    func setNotifications() {
+        guard toDoItems.count > 0 else {
+            return
+        }
+        
+        // remove all notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        // and let's re-create them with the updated data that we just saved
+        for index in 0..<toDoItems.count {
+            if toDoItems[index].reminderSet {
+                let toDoItem = toDoItems[index]
+                toDoItems[index].notificationID = setCalendarNotification(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
+            }
+            
+        }
+        
+        
+    }
+    
+    func setCalendarNotification(title: String, subtitle: String, body: String, badgeNumber: NSNumber?, sound: UNNotificationSound?, date: Date) -> String {
+        // create content:
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+        content.sound = sound
+        content.badge = badgeNumber
+        
+        // create trigger:
+        var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        dateComponents.second = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // create request:
+        let notificationID = UUID().uuidString
+        let request = UNNotificationRequest(identifier: notificationID, content: content, trigger: trigger)
+        
+        // register request with the notification center
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("🤬 ERROR: \(error.localizedDescription) Yikes, adding notification request went wrong!")
+            } else {
+                print("Notification scheduled \(notificationID), title: \(content.title)")
+            }
+        }
+        return notificationID
         
     }
     
@@ -53,7 +123,7 @@ class ToDoListViewController: UIViewController {
         } catch {
             print("🤬 ERROR: Could not save data \(error.localizedDescription)")
         }
-        
+        setNotifications()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
